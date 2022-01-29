770f66526256507302a56901f947fd68633299f8
Pavich Komansil <phavit_k@hotmail.com>
2022-01-29 11:17:12 +0700

    Complete Installer

--- Install P Browser Builder/Form1.Designer.vb
+++ Install P Browser Builder/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,14 +20,18 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -60,37 +64,55 @@ Partial Class Form1
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "P Browser Builder Installer"
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Install_P_Browser_Builder.My.Resources.Resources.P_Browser_icon_001
+        Me.PictureBox1.Location = New System.Drawing.Point(119, 99)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(305, 188)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Segoe UI Semilight", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(66, 313)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(261, 21)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "By using this application, You agree to the" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Label3.Font = New System.Drawing.Font("Segoe UI Semilight", 10.0!)
-        Me.Label3.ForeColor = System.Drawing.SystemColors.MenuHighlight
-        Me.Label3.Location = New System.Drawing.Point(12, 362)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label3.Location = New System.Drawing.Point(319, 313)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(93, 19)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Abort Installer"
+        Me.Label3.Size = New System.Drawing.Size(182, 19)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "End User License Agreement" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 344)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(528, 5)
+        Me.ProgressBar1.TabIndex = 8
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Label4.Font = New System.Drawing.Font("Segoe UI Semilight", 10.0!)
-        Me.Label4.ForeColor = System.Drawing.SystemColors.MenuHighlight
-        Me.Label4.Location = New System.Drawing.Point(464, 362)
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Light", 10.0!)
+        Me.Label4.Location = New System.Drawing.Point(229, 317)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(76, 19)
-        Me.Label4.TabIndex = 4
-        Me.Label4.Text = "I need help"
-        '
-        'WebBrowser1
-        '
-        Me.WebBrowser1.Location = New System.Drawing.Point(12, 103)
-        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.Size = New System.Drawing.Size(528, 223)
-        Me.WebBrowser1.TabIndex = 5
+        Me.Label4.Size = New System.Drawing.Size(99, 19)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Installing Data..."
         '
         'Form1
         '
@@ -98,17 +120,21 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.WhiteSmoke
         Me.ClientSize = New System.Drawing.Size(552, 393)
-        Me.Controls.Add(Me.WebBrowser1)
         Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "P Browser Builder Installer"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -117,7 +143,9 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents Label4 As Label
-    Friend WithEvents WebBrowser1 As WebBrowser
 End Class
--- Install P Browser Builder/Form1.vb
+++ Install P Browser Builder/Form1.vb
@@ -1,14 +1,43 @@
-﻿Public Class Form1
+﻿Imports System.IO.Compression
+Public Class Form1
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-        Application.Exit()
+        Process.Start("https://pavichdev.ddns.net/download/documents/p-browser-builder-eula.pdf")
     End Sub
 
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-        Process.Start("https://pavichdev.ddns.net/Home.html#feedbackintro")
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Button1.Enabled = True
+        End If
+        If CheckBox1.Checked = False Then
+            Button1.Enabled = False
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim apppath = Application.StartupPath
-        WebBrowser1.Navigate(apppath + "/documents/p-browser-builder-eula.pdf")
+        Button1.Enabled = False
+        Label4.Visible = False
+        ProgressBar1.Visible = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CheckBox1.Visible = False
+        Label3.Visible = False
+        Button1.Visible = False
+        Label4.Visible = True
+        ProgressBar1.Visible = True
+        ProgressBar1.Value = 0
+        Dim apppath As String = Application.StartupPath()
+        Dim zipPath As String = apppath + "\installresource\installresource.zip"
+        Dim extractPath As String = apppath
+        Try
+            ZipFile.ExtractToDirectory(zipPath, extractPath)
+        Catch ex As Exception
+            MessageBox.Show("Error! Please uninstall older version first!", "Installation Exist Error!")
+            Process.Start(apppath)
+            Application.Exit()
+        End Try
+        MessageBox.Show("Installation Complete!", "Complete!")
+        Process.Start(apppath + "\P Browser Builder.exe")
+        Application.Exit()
     End Sub
 End Class
