4f9205f74fbe397adf6d2b29b7239a8acbcb0794
Pavich Komansil <phavit_k@hotmail.com>
2022-03-13 17:34:23 +0700

    Remove unused resource

--- P Browser Builder/My Project/Resources.Designer.vb
+++ P Browser Builder/My Project/Resources.Designer.vb
@@ -83,26 +83,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property Screenshot_2022_03_07_130117() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Screenshot 2022-03-07 130117", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property Screenshot_2022_03_07_130137() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Screenshot 2022-03-07 130137", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property Screenshot_2022_03_13_131724() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Screenshot 2022-03-13 131724", resourceCulture)
