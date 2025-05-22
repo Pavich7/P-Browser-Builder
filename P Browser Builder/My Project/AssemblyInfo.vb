e1fbf53055a76f8747cc9f8b13655971ea2ab624
Pavich Komansil <phavit_k@hotmail.com>
2025-05-22 16:13:21 +0700

    pre-finalize 9.6

--- P Browser Builder/My Project/AssemblyInfo.vb
+++ P Browser Builder/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("9.5.0")>
-<Assembly: AssemblyFileVersion("9.5.0")>
+<Assembly: AssemblyVersion("9.6.0")>
+<Assembly: AssemblyFileVersion("9.6.0")>
