e25a418b97a78bf540236ebc309e57bc451c7b60
Pavich Komansil <phavit_k@hotmail.com>
2025-09-05 16:01:43 +0700

    update cefsharp.

--- P Browser Builder/My Project/AssemblyInfo.vb
+++ P Browser Builder/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("9.8.1")>
-<Assembly: AssemblyFileVersion("9.8.1")>
+<Assembly: AssemblyVersion("9.8.2")>
+<Assembly: AssemblyFileVersion("9.8.2")>
